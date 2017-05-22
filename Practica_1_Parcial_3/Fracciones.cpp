d5ed7818951b5381d9d0992fba5cff0125bf2d6f
Efraín Girón <efrainbond@hotmail.com>
2017-05-22 08:28:14 -0500

    Creada clase Fracciones

---
+++ Practica_1_Parcial_3/Fracciones.cpp
@@ -0,0 +1,13 @@
+#include "Fracciones.h"
+
+Fracciones::Fracciones(int a, int b, int c)
+{
+	Entero = c;
+	Numerador = b;
+	Denominador = c;
+}
+
+Fracciones::~Fracciones()
+{
+
+}
---
+++ Practica_1_Parcial_3/Fracciones.h
@@ -0,0 +1,14 @@
+//Numerador/Denominador
+#ifndef FRACCIONES_H
+#define FRACCIONES_H
+
+class Fracciones
+{
+private:
+	int Denominador, Numerador, Entero;
+public:
+	Fracciones(int a, int b, int c);
+	~Fracciones();
+};
+
+#endif //FRACCIONES_H
